eb9b6544fc4e9b0180d661e1aed07dabb15f7074
Omar Mohamed <98606902+OmarLkhalil@users.noreply.github.com>
2022-12-03 19:26:44 +0200

    Update Demo.java

--- object_oriented_core/Demo.java
+++ object_oriented_core/Demo.java
@@ -1,5 +1,30 @@
 package object_oriented_core;
 
+import java.math.BigDecimal;
+
+
 public class Demo {
 
+	public static void main(String[] args) {
+		
+		System.out.println("========== DEFAULT CONSTRUCTOR DEMO ==========");
+		
+		Cart cart = new Cart();
+		System.out.println(cart);
+		
+		System.out.println();
+		System.out.println("========== CUSTOM CONSTRUCTOR DEMO ==========");
+		
+		Cart cart2 = new Cart(2, 2);
+		System.out.println(cart2);
+		
+		System.out.println();
+		System.out.println("========== ADD PRODUCTS TO CART ==========");
+		
+		cart.addProduct(new Product("L shaped Showerbath", BigDecimal.valueOf(300)));
+		cart.addProduct(new Product("P Shaped Right Hand Bath", BigDecimal.valueOf(200)));
+
+		System.out.println(cart);
+		
+	}
 }
